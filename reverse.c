c419994544a9bc0f23d5261fe1848a7f9720231e
RAFEEK9489675090 <rafeekm1998@gmail.com>
2016-09-26 14:30:52 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -2,11 +2,20 @@
 
 int main()
 {
-  int i;
+  int n,reverse = 0;
   
-  for(i=0;i<10;i++)
+  printf("Enter a number to reverse\n");
+  scanf("%d",&n);
+  
+  while (n !=0)
   {
-    printf("%d",i);
+    transverse =transverse * 10;
+    transverse =transverse + n%10;
+    n          =n/10;
   }
+  
+  printf("Reverse of entered number is = %d\n", transverse);
+  
+  return 0:
 }
 
